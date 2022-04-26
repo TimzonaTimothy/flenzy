20a7d15e0b7a3dcabb27a8941141492f003524c9
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2022-04-26 23:50:16 +0100

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
