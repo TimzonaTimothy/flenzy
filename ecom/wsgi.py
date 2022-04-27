b48dab9be2356f21d76980404753f90ec008c0fc
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2022-04-27 10:23:48 +0100

    f

--- ecom/wsgi.py
+++ ecom/wsgi.py
@@ -12,6 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom.settings')
-from whitenoise.django import DjangoWhiteNoise
-# application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+application = get_wsgi_application()
