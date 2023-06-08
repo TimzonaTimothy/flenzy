c38f13ff821db56a1343051da202b00fa9729913
Arinze Timothy <78275394+TimzonaTimothy@users.noreply.github.com>
2023-06-08 14:23:04 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -50,7 +50,7 @@ class Account(AbstractBaseUser):
     last_login    = models.DateTimeField(auto_now_add=True)   
     is_admin      = models.BooleanField(default=False)
     is_staff      = models.BooleanField(default=False)
-    is_active     = models.BooleanField(default=False)
+    is_active     = models.BooleanField(default=True)
     is_superadmin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
